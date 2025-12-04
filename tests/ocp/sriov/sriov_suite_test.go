bc3932d54dfd4f78d9467890db69325fdc3db089
root <root@wsfd-advnetlab245.anl.eng.bos2.dc.redhat.com>
2025-12-04 15:14:04 -0500

    refactor(ocp-sriov): remove version metadata feature
    - Simplified ReportAfterSuite (removed version/metadata collection)
    - Removed version.go file (no longer needed)
    - Removed unused imports (os, sort, strings)
    - Consistent with removal of custom report functions from config

--- tests/ocp/sriov/sriov_suite_test.go
+++ tests/ocp/sriov/sriov_suite_test.go
@@ -2,10 +2,7 @@ package sriov
 
 import (
 	"fmt"
-	"os"
 	"runtime"
-	"sort"
-	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -99,81 +96,8 @@ var _ = JustAfterEach(func() {
 })
 
 var _ = ReportAfterSuite("", func(report Report) {
-	// Get cluster and operator versions for report metadata
-	var ocpVersion, sriovVersion string
-	var versionErr error
-
-	ocpVersion, versionErr = sriovenv.GetOCPVersion()
-	if versionErr != nil {
-		klog.V(90).Infof("Failed to get OCP version: %v", versionErr)
-		ocpVersion = "unknown"
-	}
-
-	sriovVersion, versionErr = sriovenv.GetSriovOperatorVersion()
-	if versionErr != nil {
-		klog.V(90).Infof("Failed to get SR-IOV operator version: %v", versionErr)
-		sriovVersion = "unknown"
-	}
-
-	// Get SR-IOV operator pod container information
-	var containerInfo []sriovenv.PodContainerInfo
-
-	containerInfo, versionErr = sriovenv.GetSriovOperatorPodContainers()
-	if versionErr != nil {
-		klog.V(90).Infof("Failed to get SR-IOV operator pod containers: %v", versionErr)
-		containerInfo = []sriovenv.PodContainerInfo{}
-	}
-
-	// Log version information
-	klog.V(90).Infof("Test Report Metadata - OCP Version: %s, SR-IOV Operator Version: %s", ocpVersion, sriovVersion)
-
-	// Create the report
 	reportPath := SriovOcpConfig.GetReportPath()
 	reportxml.Create(report, reportPath, SriovOcpConfig.TCPrefix)
-
-	// Write version metadata to a separate file
-	if reportPath != "" {
-		metadataPath := strings.TrimSuffix(reportPath, ".xml") + "_metadata.txt"
-
-		var metadataBuilder strings.Builder
-		metadataBuilder.WriteString(fmt.Sprintf("OpenShift Cluster Version: %s\n", ocpVersion))
-		metadataBuilder.WriteString(fmt.Sprintf("SR-IOV Operator Version: %s\n", sriovVersion))
-		metadataBuilder.WriteString(fmt.Sprintf("Report Generated: %s\n", report.EndTime.Format("2006-01-02 15:04:05")))
-		metadataBuilder.WriteString("\nSR-IOV Operator Pod Containers:\n")
-
-		if len(containerInfo) > 0 {
-			// Group by pod name for better readability
-			podMap := make(map[string][]sriovenv.PodContainerInfo)
-			for _, info := range containerInfo {
-				podMap[info.PodName] = append(podMap[info.PodName], info)
-			}
-
-			podNames := make([]string, 0, len(podMap))
-			for podName := range podMap {
-				podNames = append(podNames, podName)
-			}
-
-			sort.Strings(podNames)
-
-			for _, podName := range podNames {
-				containers := podMap[podName]
-				metadataBuilder.WriteString(fmt.Sprintf("\n  Pod: %s\n", podName))
-
-				for _, container := range containers {
-					metadataBuilder.WriteString(fmt.Sprintf("    Container: %s\n", container.ContainerName))
-					metadataBuilder.WriteString(fmt.Sprintf("    Image: %s\n", container.Image))
-				}
-			}
-		} else {
-			metadataBuilder.WriteString("  (No container information available)\n")
-		}
-
-		if err := os.WriteFile(metadataPath, []byte(metadataBuilder.String()), 0600); err != nil {
-			klog.Errorf("Failed to write metadata file %s: %v", metadataPath, err)
-		} else {
-			klog.V(90).Infof("Version metadata written to: %s", metadataPath)
-		}
-	}
 
 	// Print report file locations
 	junitReportPath := SriovOcpConfig.GetJunitReportPath(currentFile)
@@ -182,10 +106,8 @@ var _ = ReportAfterSuite("", func(report Report) {
 
 	if reportPath != "" {
 		fmt.Printf("Test Run Report: %s\n", reportPath)
-		fmt.Printf("Metadata File: %s\n", strings.TrimSuffix(reportPath, ".xml")+"_metadata.txt")
 	} else {
 		fmt.Printf("Test Run Report: (disabled - set ECO_ENABLE_REPORT=true to enable)\n")
-		fmt.Printf("Metadata File: (disabled - set ECO_ENABLE_REPORT=true to enable)\n")
 	}
 
 	fmt.Printf("========================\n\n")
